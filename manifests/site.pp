19f8a68b61a91c5679fd2b6cda7b9e8b21121522
Alex Fonts <alejandro.escom@gmail.com>
2021-07-22 02:21:43 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,10 @@
 node default {
-  file {'/root/README':
-    ensure => file,
-    content => 'This is a REAME',
-    owner => 'root',
-  }
+#  file {'/root/README':
+#    ensure => file,
+#    content => 'This is a REAME',
+#    owner => 'root',
+#  }
+}
+node 'master.puppet.vm' {
+  include role::master_server
 }
